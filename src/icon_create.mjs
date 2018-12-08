1ea615bab41d79e2891bfcefc74c73f4bce347d1
Vincent Vallet <vincent.vallet@gmail.com>
2018-12-08 18:39:16 +0100

    clearer icons

--- src/icon_create.mjs
+++ src/icon_create.mjs
@@ -13,10 +13,11 @@ const angles = {
 const SVG_START = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve">
-<polygon points="33.4,9.9 66.6,9.9 90.1,33.4 90.1,66.6 66.6,90.1 33.4,90.1 9.9,66.6 9.9,33.4" fill="#000000" />`
+<polygon points="30.364,2 69.636,2 98,30.364 98,69.636 69.636,98 30.364,98 2,69.636 2,30.364" fill="none" stroke="white" stroke-width="4" />
+<polygon points="39.773,25 60.227,25 75,39.773 75,60.227 60.227,75 39.773,75 25,60.227 25,39.773" fill="black" />`;
 
 // Path for an orientation
-const ORIENTATION = `<path d="m 29.4,0 l 4,9.6 l 33.2,0 l 4,-9.6 z" fill="#000000" id="svg_1" transform="rotate(_ANGLE_ 50 50)"/>`;
+const ORIENTATION = `<polygon points="33.2,6 66.8,6 60,23 40,23" fill="black" transform="rotate(_ANGLE_ 50 50)"/>`;
 
 const SVG_END = `</svg>`;
 
